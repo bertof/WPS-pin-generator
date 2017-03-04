cf9b5a49670d9f6ea18d323803f29076f50a66ed
Filippo Berto <filippoberto95@gmail.com>
2017-03-04 08:59:51 +0100

    Fixed order and added prompts

--- main.cpp
+++ main.cpp
@@ -11,14 +11,24 @@ int main(int argc, char *argv[]) {
 	//TODO better spash screen
 	//TODO better input handling
 	//TODO guide on how to use it (--help)
-	InputHandler::handle(argc, argv);
 
+	// Spash screen
 	std::cout << SplashScreen::getSplashScreen() << std::endl;
 
+	// Handle input
+	InputHandler::handle(argc, argv);
+
+	// Choose the generator
 	GeneratorInterface *g = new bertof::Generator();
 
+	// Generate pins
 	g->generatePin("7C:5C:F8:F6:D0:B5");
 
+	// TODO show the pins
+	std::cout << "Solution:\t" << std::endl;
+
+
+	// END
 	std::cout << "END MAIN" << std::endl;
 
 	return 0;
