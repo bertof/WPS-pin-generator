6e2a24c12e9ab5a4373e2fc57549307cb607b03e
Filippo Berto <filippoberto95@gmail.com>
2017-03-15 08:11:03 +0100

    Better help info and argument handling

--- Graphics/HelpScreen/HelpScreen.cpp
+++ Graphics/HelpScreen/HelpScreen.cpp
@@ -4,16 +4,26 @@
 
 #include "HelpScreen.h"
 #include "../SplashScreen/SplashScreen.h"
-
 #include "../Info/Info.h"
 
+#include <fstream>
+
 std::string HelpScreen::getHelpScreen() {
-	return "Version: " + Info::getVersion() + "\n" +
-		"Usage: " + Info::getExecutableName() + " [options]\n" +
-		"OPTIONS:\n" +
-		"-b, --bssid <BSSID>\t\tInput target BSSID\n" +
-		"-h, --help\t\t\t\tPrint this help page\n" +
-		"--debug\t\t\t\t\tPrint all th debug info" +
-		"-v, --verbouse\tLog is more verbouse\n" +
-		"--logfile [log.txt]\t\tEnables log to file (default log.txt)\n";
+	return
+		"Version: " + Info::getVersion() + "\n" +
+			"USAGE:\n" +
+			Info::getExecutableName() + " [options]\n" +
+			"\n" +
+			"OPTIONS:\n" +
+			"-b, --bssid <BSSID>\t\tInput target BSSID\n" +
+			"-g\t\t\t\t\t\tUse the generator algorithm\n" +
+			"-d\t\t\t\t\t\tUse the database\n" +
+			"-h, --help\t\t\t\tPrint this help page\n" +
+			"--debug\t\t\t\t\tPrint all th debug info" +
+			"-v, --verbouse\tLog is more verbouse\n" +
+			"--logfile [log.txt]\t\tEnables log to file (default log.txt)\n" +
+			"\n" +
+			"EXAMPLES:\n" +
+			Info::getExecutableName() + " -h\n" +
+			Info::getExecutableName() + " -b 00:11:22:33:44:55 -g -d --logfile";
 }
--- InputHandling/InputHandler.cpp
+++ InputHandling/InputHandler.cpp
@@ -12,6 +12,7 @@
 #include "../Generator/GeneratorInerface.h"
 #include "../Generator/bertofGenerator/Generator.h"
 #include "../Exceptions/NotImplementedException.h"
+#include "../Exceptions/HelpScreenException.h"
 
 #include <regex>
 #include <iostream>
@@ -43,166 +44,166 @@ void InputHandler::handle(int argc, char **argv) {
 
 		// Default behaviour when not using any aguments: show help screen
 		if (args.size() == 1) {
-			std::cout << HelpScreen::getHelpScreen() << std::endl;
+			throw HelpScreenException();
+		}
+
+		// Loop starts from 1 as args[0] allways contains the executable path
+		int i = 1;
+		while (i < args.size()) {
+
+			// SWITCHES
+
+			// Activate verbouse mode
+			if (args[i] == "--verbouse" || args[i] == "-v") {
+				Logger::setVerbouseLogActive(true);
+				++i;
+			}
+
+				// Activate debug mode
+			else if (args[i] == "--debug") {
+				Logger::setDebugLogActive(true);
+				++i;
+			}
+
+				// Activate write on log file
+			else if (args[i] == "--logfile") {
+				TextLogger::setWriteOnFileActive(true);
+
+				// If a path is passed nex uses it as the log file path
+				if (i + 1 < args.size() && std::regex_match(args[i + 1], std::regex("^(^-)"))) {
+					TextLogger::getTextLogger(args[i + 1]);
+					++i;
+				}
+
+				++i;
+			}
+
+				// Help dialog
+			else if (args[i] == "--help" || args[i] == "-h") {
+				throw HelpScreenException();
+			}
+
+				// BSSID input
+			else if (args[i] == "-b" || args[i] == "--bssid") {
+
+				// Listen for a BSSID as next argument
+
+				// While i find arguments that match a BSSID format
+				while (i + 1 < args.size()
+					&& std::regex_match(args[i + 1], std::regex("^(([0-9A-Fa-f]){2}:){5}([0-9a-fA-F]){2}$"))) {
+
+					bssid_arguments.push_back(args[i + 1]);
+					++i;
+				}
+
+				// Check if any BSSID is been given
+				if (bssid_arguments.size() == 0) {
+					logger->logError("No BSSID found");
+					throw InvalidInputException("No BSSID found");
+				}
+
+				++i;
+			}
+
+				// ASSID input
+			else if (args[i] == "-a" || args[i] == "--assid") {
+
+				// Listen for a ASSID as next argument
+
+				// While i find arguments that match a BSSID format
+				while (i + 1 < args.size() &&
+					std::regex_match(args[i + 1], std::regex("^(^-)"))) {
+					bssid_arguments.push_back(args[i + 1]);
+					++i;
+				}
+
+				// Check if any ASSID is been given
+				if (assid_arguments.size() == 0) {
+					logger->logError("No ASSID found");
+					throw InvalidInputException("No ASSID found");
+				}
+
+				++i;
+			}
+
+				// Switch to enable generator results
+			else if (args[i] == "-g" || args[i] == "--generator") {
+				useGenerator = true;
+				++i;
+			}
+
+				// Switch to enable database results
+			else if (args[i] == "-d" || args[i] == "--database") {
+				useDatabase = true;
+				++i;
+			}
+
+				// Case the argument is invalid
+			else {
+				throw InvalidInputException(args[i]);
+			}
+
+		} // End of for cycle
+
+		// Output generation
+		//TODO output generation
+
+		if (useGenerator || useDatabase) {
+			logger->log("Results:");
 		} else {
+			logger->logError("-g or -d options not set.");
+			throw InvalidInputException();
+		}
 
-			// Loop starts from 1 as args[0] allways contains the executable path
-			int i = 1;
-			while (i < args.size()) {
-
-				// SWITCHES
-
-				// Activate verbouse mode
-				if (args[i] == "--verbouse" || args[i] == "-v") {
-					Logger::setVerbouseLogActive(true);
-					++i;
-				}
-
-					// Activate debug mode
-				else if (args[i] == "--debug") {
-					Logger::setDebugLogActive(true);
-					++i;
-				}
-
-					// Activate write on log file
-				else if (args[i] == "--logfile") {
-					TextLogger::setWriteOnFileActive(true);
-
-					// If a path is passed nex uses it as the log file path
-					if (i + 1 < args.size() && std::regex_match(args[i + 1], std::regex("^(^-)"))) {
-						TextLogger::getTextLogger(args[i + 1]);
-						++i;
-					}
-
-					++i;
-				}
-
-					// Help dialog
-				else if (args[i] == "--help" || args[i] == "-h") {
-					std::cout << HelpScreen::getHelpScreen() << std::endl;
-					break;
-				}
-
-					// BSSID input
-				else if (args[i] == "-b" || args[i] == "--bssid") {
-
-					// Listen for a BSSID as next argument
-
-					// While i find arguments that match a BSSID format
-					while (i + 1 < args.size()
-						&& std::regex_match(args[i + 1], std::regex("^(([0-9A-Fa-f]){2}:){5}([0-9a-fA-F]){2}$"))) {
-
-						bssid_arguments.push_back(args[i + 1]);
-						++i;
-					}
-
-					// Check if any BSSID is been given
-					if (bssid_arguments.size() == 0) {
-						logger->logError("No BSSID found");
-						throw InvalidInputException("No BSSID found");
-					}
-
-					++i;
-				}
-
-					// ASSID input
-				else if (args[i] == "-a" || args[i] == "--assid") {
-
-					// Listen for a ASSID as next argument
-
-					// While i find arguments that match a BSSID format
-					while (i + 1 < args.size() &&
-						std::regex_match(args[i + 1], std::regex("^(^-)"))) {
-						bssid_arguments.push_back(args[i + 1]);
-						++i;
-					}
-
-					// Check if any ASSID is been given
-					if (assid_arguments.size() == 0) {
-						logger->logError("No ASSID found");
-						throw InvalidInputException("No ASSID found");
-					}
-
-					++i;
-				}
-
-					// Switch to enable generator results
-				else if (args[i] == "-g" || args[i] == "--generator") {
-					useGenerator = true;
-					++i;
-				}
-
-					// Switch to enable database results
-				else if (args[i] == "-d" || args[i] == "--database") {
-					useDatabase = true;
-					++i;
-				}
-
-					// Case the argument is invalid
-				else {
-					throw InvalidInputException(args[i]);
-				}
-
-			} // End of for cycle
-
-			// Output generation
-			//TODO output generation
-
-			if (useGenerator || useDatabase) {
-				logger->log("Results:");
-			} else {
-				logger->logError("-g or -d options not set.");
-			}
-
-			if (useGenerator) {
-				std::shared_ptr<GeneratorInterface> generator(new bertof::Generator());
-				logger->logVerbouse(
-					"Results from generator by " + generator->author() + " version " + generator->version());
-				for (int k = 0; k < bssid_arguments.size(); ++k) {
-					std::vector<Pin> results = generator->generatePin(bssid_arguments[k]);
-					logger->log("BSSID:\t" + bssid_arguments[k]);
-					logger->logVerbouse("Pins generated:");
-					for (int i = 0; i < results.size(); ++i) {
-						logger->log(results[i].toString());
-					}
-				}
-			}
-
-			if (useDatabase) {
-				/*
-
-				 DatabaseInterface *database = new bertof::Database();
-				logger->logVerbouse("Results from database by " + database->author() + " version " + database->version());
-
-				for (int k = 0; k < bssid_arguments.size(); ++k) {
-				 std::vector<Pin> results = generator.generatePin(bssid_arguments[k]);
-				 logger->log(bssid_arguments[k] + " results:");
-				 for (int i = 0; i < results.size(); ++i) {
+		if (useGenerator) {
+			std::shared_ptr<GeneratorInterface> generator(new bertof::Generator());
+			logger->logVerbouse(
+				"Results from generator by " + generator->author() + " version " + generator->version());
+			for (int k = 0; k < bssid_arguments.size(); ++k) {
+				std::vector<Pin> results = generator->generatePin(bssid_arguments[k]);
+				logger->log("BSSID:\t" + bssid_arguments[k]);
+				logger->logVerbouse("Pins generated:");
+				for (int i = 0; i < results.size(); ++i) {
 					logger->log(results[i].toString());
-				 }
 				}
-
-				for (int k = 0; k < assid_arguments.size(); ++k) {
-				 std::vector<Pin> results = generator.generatePin(assid_arguments[k]);
-				 logger->log(bssid_arguments[k] + " results:");
-				 for (int i = 0; i < results.size(); ++i) {
-					logger->log(results[i].toString());
-				 }
-				}
-
-				*/
-
-				logger->logError("Database is not implemented yet");
-				throw NotImplementedException("Database is not implemented yet");
 			}
+		}
+
+		if (useDatabase) {
+			/*
+
+			 DatabaseInterface *database = new bertof::Database();
+			logger->logVerbouse("Results from database by " + database->author() + " version " + database->version());
+
+			for (int k = 0; k < bssid_arguments.size(); ++k) {
+			 std::vector<Pin> results = generator.generatePin(bssid_arguments[k]);
+			 logger->log(bssid_arguments[k] + " results:");
+			 for (int i = 0; i < results.size(); ++i) {
+				logger->log(results[i].toString());
+			 }
+			}
+
+			for (int k = 0; k < assid_arguments.size(); ++k) {
+			 std::vector<Pin> results = generator.generatePin(assid_arguments[k]);
+			 logger->log(bssid_arguments[k] + " results:");
+			 for (int i = 0; i < results.size(); ++i) {
+				logger->log(results[i].toString());
+			 }
+			}
+
+			*/
+
+			logger->logError("Database is not implemented yet");
+			throw NotImplementedException("Database is not implemented yet");
 		}
 
 	} catch (InvalidInputException e) {
 		std::shared_ptr<DoubleLogger> logger(DoubleLogger::getDoubleLogger());
 		logger->logError("Argument passed is invalid:\t" + std::string(e.what()));
 		throw InvalidInputException("Argument passed is invalid:\t\"" + std::string(e.what()) + "\"");
+	} catch (HelpScreenException e) {
+		std::cout << HelpScreen::getHelpScreen() << std::endl;
 	}
-
 }
 
 const std::string &InputHandler::getBssidInput() {
